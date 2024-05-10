0b7948f7f8746328db776b260217d16ae309a896
songtao.gong <songtao.gong@saxofintech.com>
2024-05-10 11:29:41 +0800

    fix root path

--- utils/root_path.py
+++ utils/root_path.py
@@ -6,5 +6,7 @@ DEFAULT_PATH = Path(__file__).resolve().parent.parent
 
 """Return config folder."""
 DEFAULT_CONFIG_PATH = DEFAULT_PATH / "config"
+if not Path(DEFAULT_CONFIG_PATH).is_dir():
+    Path(DEFAULT_CONFIG_PATH).mkdir(exist_ok=True)
 
 
