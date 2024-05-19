a2eb3ab416a3ea63390dcc851725803e261f52fb
ez4leon <565564274@qq.com>
2024-05-19 23:01:41 +0800

    add skip version check func

---
+++ skip.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pymem import Pymem
+
+
+ADDRS = [0x2FFEAF8, 0x3020E1C, 0x3021AEC, 0x303C4D8, 0x303FEF4, 0x3040FA4, 0x30416EC]
+
+
+def fix_version(pm: Pymem):
+    WeChatWindll_base = 0
+    for m in list(pm.list_modules()):
+        path = m.filename
+        if path.endswith("WeChatWin.dll"):
+            WeChatWindll_base = m.lpBaseOfDll
+            break
+
+    for offset in ADDRS:
+        addr = WeChatWindll_base + offset
+        v = pm.read_uint(addr)
+        if v == 0x63090A13:  # 已经修复过了
+            continue
+        elif v != 0x63090217:  # 不是 3.9.2.23 修复也没用
+            raise Exception("别修了，版本不对，修了也没啥用。")
+
+        pm.write_uint(addr, 0x63090A13)
+
+    print("好了，可以扫码登录了")
+
+
+if __name__ == "__main__":
+    try:
+        pm = Pymem("WeChat.exe")
+        fix_version(pm)
+    except Exception as e:
+        print(f"{e}，请确认微信程序已经打开！")
